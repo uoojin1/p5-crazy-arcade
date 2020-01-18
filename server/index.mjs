8ec72cdb7e9cd254df63247a94a2c2cd14e8e416
Eugene Moon <uoojin95@gmail.com>
2020-01-18 18:30:48 -0500

    ✨ Feat: adding morgan for the request loggin

--- server/index.mjs
+++ server/index.mjs
@@ -1,9 +1,14 @@
 import express from 'express'
 import path from 'path'
+import logger from 'morgan'
 
+// define port for the express app
+const port = process.env.PORT || 3000
+
+// create express app
 const app = express()
 
-const port = process.env.PORT || 3000
+app.use(logger('common'))
 
 const __dirname = path.resolve(path.dirname(''))
 const DIST_DIR = path.join(__dirname, 'build')
