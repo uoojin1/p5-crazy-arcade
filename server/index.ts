f9c29531b46a17f7ca1e02e3771bbcd1af728bdd
Eugene Moon <uoojin95@gmail.com>
2020-01-19 18:56:23 -0500

    🐛 Fix: trying to debug the memory issue

--- server/index.ts
+++ server/index.ts
@@ -12,13 +12,21 @@ const port = process.env.PORT || 3000
 
 // create express app
 const app = express()
+let used = process.memoryUsage().heapUsed / 1024 / 1024;
+console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
 
 // create the httpsever with the express app
 const server = http.createServer(app)
+used = process.memoryUsage().heapUsed / 1024 / 1024;
+console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
 
 const wss = io(server)
+used = process.memoryUsage().heapUsed / 1024 / 1024;
+console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
 
 app.use(logger('common'))
+used = process.memoryUsage().heapUsed / 1024 / 1024;
+console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
 
 const __dirname = path.resolve(path.dirname(''))
 const DIST_DIR = path.join(__dirname, 'build')
@@ -37,3 +45,5 @@ wssController(wss)
 server.listen(port, () => {
   console.log('App listening on port: ' + port);
 })
+used = process.memoryUsage().heapUsed / 1024 / 1024;
+console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
