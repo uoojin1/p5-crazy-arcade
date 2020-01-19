ab5bd977d1095e8c08ec61d2ffa9f7b02bc0e1a9
Eugene Moon <uoojin95@gmail.com>
2020-01-18 19:34:59 -0500

    ✨ Feat: added type def files for p5

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,7 +1,22 @@
 import * as React from 'react'
+import P5Canvas from 'react-p5-wrapper'
+import * as p5 from 'p5'
+
+export function sketch(p5: p5) {
+  p5.setup = function() {
+    // empty setup
+  }
+
+  p5.draw = function() {
+    p5.ellipse(50, 50, 50, 50)
+  }
+}
 
 export const Home = () => {
   return (
-    <div>HOME</div>
+    <div>
+      <h1>HOME</h1>
+      <P5Canvas sketch={sketch} />
+    </div>
   )
 }
