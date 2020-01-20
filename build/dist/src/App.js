6f3ee1ce007aa150c9b8f2e8478ce93c1e6ffef6
Eugene Moon <uoojin95@gmail.com>
2020-01-19 20:21:08 -0500

    Revert "🐛 Fix: trying to see if adding the build files will fix the issue"
    This reverts commit 3654922bac80b77d31e4c17ce10a93f870ae8d7b.

--- build/dist/src/App.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var React = require("react");
-var Home_1 = require("./components/Home");
-exports.App = function () {
-    return (React.createElement(Home_1.Home, null));
-};
-//# sourceMappingURL=App.js.map
